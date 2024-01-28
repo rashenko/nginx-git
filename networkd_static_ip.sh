ff74c2e7d2b0a3cd8d6491a3f1cc46811634f935
rashenko <149801050+rashenko@users.noreply.github.com>
2024-01-28 17:46:57 +0300

    networkd_static_ip.sh

--- networkd_static_ip.sh
+++ networkd_static_ip.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 DIR='/etc/systemd/network/'
-DIR1='/home/qqee/nginx-git/10-eth.network'
+DIR1='/home/qqee/network/10-eth.network'
 DIR2='/etc/systemd/network/10-eth.network'
 
+# Копируем конфиг
 cp $DIR1 $DIR2
 echo "Конфиг 10-eth.network, Networkd сервиса скопирован по пути $DIR"
 
+# Перезапускаем сервис
 systemctl restart systemd-networkd
 if [ $? -eq 0 ]; then
         echo "Сервис Network включил Вам статический ip адрес!"
