97da0e2e0f05e4806274a3633c7669bc46b58835
rashenko <149801050+rashenko@users.noreply.github.com>
2024-01-28 17:33:44 +0300

    node_exporter.sh

---
+++ node_exporter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Скачиваем node_exporter для мониторнинга, можно через wget
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+
+# Распаковка архивов 
+tar xzvf node_exporter-*.t*gz
+
+# Добавляем пользователей
+useradd --no-create-home --shell /bin/false node_exporter
+
+# Копируем файлы в /usr/local
+cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin
+chown node_exporter: /usr/local/bin/node_exporter
+
+# Копируем конфиг службы node exporter
+cp /home/qqee/nginx-git/node_exporter.service /etc/systemd/system/node_exporter.service
+
+# Перезапуск
+systemctl daemon-reload
+
+# Запускаем службу node_exporter
+systemctl start node_exporter
+
+# Проверяем статус службы node_exporter
+systemctl status node_exporter
+
+# Включаем автозапуск службы, при включении машины
+systemctl enable node_exporter
+
+# Удаляем установочные пакеты
+rm -r node_exporter-1.6.1.linux-amd64.tar.gz
+rm -r node_exporter-1.6.1.linux-amd64
