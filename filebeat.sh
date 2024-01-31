9e6bd1bde2140e91375d8250b77952e9cfc082f0
rashenko <149801050+rashenko@users.noreply.github.com>
2024-01-31 12:44:17 +0300

    filebeat.sh

--- filebeat.sh
+++ filebeat.sh
@@ -2,33 +2,27 @@
 
 file_path="/etc/filebeat/filebeat.yml"
 module_path="/etc/filebeat/modules.d/nginx.yml"
+gz="prospector.scanner.exclude_files: ['.gz$']"
 
 # Замена строк в файле
-sed -i 's|- type: .*|- type: log|g' "$file_path"
-sed -i 's|  enabled: .*|  enabled: true|g' "$file_path"
-sed -i 's|    - /var/log/[^.]*.log|    - /var/log/nginx/*.log|g' "$file_path"
-sed -i 's|#prospector.scanner.exclude_files: .*|  prospector.scanner.exclude_files: \['.gz$'\]|g' "$file_path"
-sed -i 's|output.elasticsearch:|#output.elasticsearch:|g' "$file_path"
-sed -i 's|#output.logstash:|output.logstash:|g' "$file_path"
-sed -i 's|  #hosts: .*|  hosts: ["192.168.1.14:5400"]|g' "$file_path"
+sed -i "s|- type: .*|- type: log|g" "$file_path"
+sed -i "s|  enabled: .*|  enabled: true|g" "$file_path"
+sed -i "s|    - /var/log/[^.]*.log|    - /var/log/nginx/*.log|g" "$file_path"
+sed -i "s|#prospector.scanner.exclude_files: .*|$gz|g" "$file_path"
+sed -i "s|output.elasticsearch:|#output.elasticsearch:|g" "$file_path"
+sed -i "s|#output.logstash:|output.logstash:|g" "$file_path"
+sed -i "s|  #hosts: .*|  hosts: ["10.210.5.244:5400"]|g" "$file_path"
 
 # Добавление параметра exclude_files в конфигурационный файл, если он отсутствует
 if [ -f "$file_path" ]; then
     if ! grep -q "exclude_files" "$file_path"; then
-        echo "exclude_files: \['.gz$'\]" >> "$File_path"
+        echo "exclude_files: \['.gz$'\]" >> "$file_path"
     fi
 fi
 
-filebeat modules enable nginx
-
 # Замена строк в файле
-sed -i 's|  error:\n
-    enabled: .*|  error:\n
-    enabled: true|g' "$module_path"
-
-sed -i 's|  access:\n
-    enabled: .*|  access:\n
-    enabled: true|g' "$module_path"
+sed -i '/error:/,/enabled:/ s/enabled: .*/enabled: true/g' "$module_path"
+sed -i '/access:/,/enabled:/ s/enabled: .*/enabled: true/g' "$module_path"
 
 # Запуск сервиса
 systemctl restart filebeat
