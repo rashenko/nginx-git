1316334d3565b7978adc25e359465bb5551a0e80
rashenko <149801050+rashenko@users.noreply.github.com>
2024-02-01 00:05:44 +0300

    filebeat.sh

--- filebeat.sh
+++ filebeat.sh
@@ -11,7 +11,7 @@ sed -i "s|    - /var/log/[^.]*.log|    - /var/log/nginx/*.log|g" "$file_path"
 sed -i "s|#prospector.scanner.exclude_files: .*|$gz|g" "$file_path"
 sed -i "s|output.elasticsearch:|#output.elasticsearch:|g" "$file_path"
 sed -i "s|#output.logstash:|output.logstash:|g" "$file_path"
-sed -i "s|  #hosts: .*|  hosts: ["10.210.5.244:5400"]|g" "$file_path"
+sed -i "s|  #hosts: .*|  hosts: ["192.168.1.14:5400"]|g" "$file_path"
 
 # Добавление параметра exclude_files в конфигурационный файл, если он отсутствует
 if [ -f "$file_path" ]; then
