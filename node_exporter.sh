fc80f2439cbb2dbae39d440ec87121448c385296
rashenko <149801050+rashenko@users.noreply.github.com>
2024-01-31 20:58:55 +0300

    node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -3,12 +3,8 @@
 # Добавляем пользователей
 useradd --no-create-home --shell /bin/false node_exporter
 
-# Копируем файлы в /usr/local
-cp /home/qqee/node_exporter-*.linux-amd64/node_exporter /usr/local/bin
-chown node_exporter: /usr/local/bin/node_exporter
-
 # Копируем конфиг службы node exporter
-cp /home/qqee/nginx-git/node_exporter.service /etc/systemd/system/node_exporter.service
+cp /home/qqee/backend1-git/node_exporter.service /etc/systemd/system/node_exporter.service
 
 # Перезапуск
 systemctl daemon-reload
@@ -21,7 +17,3 @@ systemctl status node_exporter
 
 # Включаем автозапуск службы, при включении машины
 systemctl enable node_exporter
-
-# Удаляем установочные пакеты
-rm -r node_exporter-1.6.1.linux-amd64.tar.gz
-rm -r node_exporter-1.6.1.linux-amd64
